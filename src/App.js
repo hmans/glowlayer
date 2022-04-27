ea9577f106286e941c8c74e56962d5362620ae5b
Hendrik Mans <hendrik@mans.de>
2022-04-27 14:41:52 +0200

    UnrealBloomPass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Vector2 } from "three";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
 
 const Spaceship = () => {
   const gltf = useGLTF("/models/spaceship26.gltf");
@@ -7,8 +11,25 @@ const Spaceship = () => {
 };
 
 const Scene = () => {
-  useFrame(({ scene, camera, gl }) => {
-    gl.render(scene, camera);
+  let composer;
+
+  useFrame(({ gl, scene, camera }) => {
+    if (!composer) {
+      composer = new EffectComposer(gl);
+
+      const renderPass = new RenderPass(scene, camera);
+      composer.addPass(renderPass);
+
+      const unrealPass = new UnrealBloomPass(
+        new Vector2(256, 256),
+        2,
+        0.8,
+        0.2
+      );
+      composer.addPass(unrealPass);
+    }
+
+    composer.render();
   }, 1);
 
   return (
