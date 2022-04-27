24af764855b56fdc08a9657b12a868d4164570b3
Hendrik Mans <hendrik@mans.de>
2022-04-27 19:05:29 +0200

    Nice

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   LinearEncoding,
   WebGLRenderTarget,
   Vector2,
+  UnsignedByteType,
 } from "three";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
@@ -16,7 +17,11 @@ import { LuminosityShader } from "three/examples/jsm/shaders/LuminosityShader.js
 
 const Spaceship = () => {
   const gltf = useGLTF("/models/spaceship26_mod.gltf");
-  gltf.materials["Imphenzia"].emissiveIntensity = 20;
+
+  /* The emissive intensity in the GLTF is always capped to 1,
+     so let's increase it manually */
+  gltf.materials["Imphenzia"].emissiveIntensity = 2;
+
   return <primitive object={gltf.scene} />;
 };
 
@@ -25,9 +30,6 @@ const Scene = () => {
 
   useFrame(({ gl, scene, camera }) => {
     if (!composer) {
-      /* Glow Layer */
-
-      /* Our actual layer */
       composer = new EffectComposer(
         gl,
         new WebGLRenderTarget(window.innerWidth, window.innerHeight, {
@@ -37,13 +39,13 @@ const Scene = () => {
       );
 
       const sceneRenderPass = new RenderPass(scene, camera);
-      const unrealPass = new UnrealBloomPass(new Vector2(256, 256), 1.25, 1, 1);
+      const unrealPass = new UnrealBloomPass(new Vector2(256, 256), 3, 1, 0.8);
       const bloomPass = new BloomPass();
-
       const luminosityPass = new ShaderPass(LuminosityShader);
+
       composer.addPass(sceneRenderPass);
-      composer.addPass(luminosityPass);
-      // composer.addPass(unrealPass);
+      // composer.addPass(luminosityPass);
+      composer.addPass(unrealPass);
       // composer.addPass(bloomPass);
     }
 
@@ -53,9 +55,25 @@ const Scene = () => {
   return (
     <>
       <ambientLight intensity={0.2} />
-      <directionalLight intensity={0.8} />
+      <directionalLight intensity={1} position={[10, 10, 10]} />
       <PerspectiveCamera position-z={30} makeDefault />
       <OrbitControls />
+
+      {/* A mesh with emission */}
+      <mesh position={[-14, 0, 0]}>
+        <dodecahedronGeometry />
+        <meshStandardMaterial
+          color="white"
+          emissive="white"
+          emissiveIntensity={3}
+        />
+      </mesh>
+
+      {/* A mesh with no emission */}
+      <mesh position={[+14, 0, 0]}>
+        <dodecahedronGeometry />
+        <meshStandardMaterial color="white" />
+      </mesh>
 
       <Spaceship />
     </>
