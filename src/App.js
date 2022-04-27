f2b9e474713d5e725afbbc4b5d1e4cd82ac8f5d8
Hendrik Mans <hendrik@mans.de>
2022-04-27 13:58:49 +0200

    Three

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-export const App = () => <p>hi</p>;
+import { Box, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+export const App = () => (
+  <Canvas>
+    <color args={["black"]} attach="background" />
+    <ambientLight />
+    <OrbitControls />
+
+    <Box>
+      <meshStandardMaterial color="orange" />
+    </Box>
+  </Canvas>
+);
