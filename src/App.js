16b71110a70a245334031101ab01668987a19815
Hendrik Mans <hendrik@mans.de>
2022-04-27 14:14:57 +0200

    Spaceship!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { Box, OrbitControls } from "@react-three/drei";
+import {
+  Box,
+  OrbitControls,
+  PerspectiveCamera,
+  useGLTF,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+
+const Spaceship = () => {
+  const gltf = useGLTF("/models/spaceship26.gltf");
+  return <primitive object={gltf.scene} />;
+};
+
+const Scene = () => {
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <directionalLight intensity={0.8} />
+      <PerspectiveCamera position-z={30} makeDefault />
+      <OrbitControls />
+      <Spaceship />
+    </>
+  );
+};
 
 export const App = () => (
   <Canvas>
     <color args={["black"]} attach="background" />
-    <ambientLight />
-    <OrbitControls />
-
-    <Box>
-      <meshStandardMaterial color="orange" />
-    </Box>
+    <Scene />
   </Canvas>
 );
