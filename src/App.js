9378d5e3ab9674c96ebe16b224a4c11ad88e86fd
Hendrik Mans <hendrik@mans.de>
2022-04-28 09:42:30 +0200

    Use JSX for effects

--- src/App.js
+++ src/App.js
@@ -1,87 +1,26 @@
-import { OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
 import {
-  HalfFloatType,
-  LinearEncoding,
-  Vector2,
-  WebGLRenderTarget,
-} from "three";
+  Effects,
+  OrbitControls,
+  PerspectiveCamera,
+  useGLTF,
+} from "@react-three/drei";
+import { Canvas, extend } from "@react-three/fiber";
+import { HalfFloatType, LinearEncoding, Vector2 } from "three";
 import { AdaptiveToneMappingPass } from "three/examples/jsm/postprocessing/AdaptiveToneMappingPass.js";
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
-import { LuminosityShader } from "three/examples/jsm/shaders/LuminosityShader.js";
 import { VignetteShader } from "three/examples/jsm/shaders/VignetteShader.js";
 import { Skybox } from "./Skybox";
+
+extend({ UnrealBloomPass, AdaptiveToneMappingPass, ShaderPass });
 
 const Spaceship = () => {
   const gltf = useGLTF("/models/spaceship26_mod.gltf");
 
-  gltf.materials["Imphenzia"].emissiveIntensity = 1.2;
+  /* GLTF caps emissivion values at 1, but we can boost it manually */
+  gltf.materials["Imphenzia"].emissiveIntensity = 1.3;
 
   return <primitive object={gltf.scene} />;
-};
-
-const Scene = () => {
-  let composer;
-
-  useFrame(({ gl, scene, camera }) => {
-    if (!composer) {
-      composer = new EffectComposer(
-        gl,
-        new WebGLRenderTarget(window.innerWidth, window.innerHeight, {
-          type: HalfFloatType,
-          encoding: LinearEncoding,
-        })
-      );
-
-      const sceneRenderPass = new RenderPass(scene, camera);
-      const unrealPass = new UnrealBloomPass(
-        new Vector2(window.innerWidth, window.innerHeight),
-        1.5,
-        0,
-        0.8
-      );
-      const luminosityPass = new ShaderPass(LuminosityShader);
-      const toneMappingPass = new AdaptiveToneMappingPass(true, 256);
-      const vignettePass = new ShaderPass(VignetteShader);
-
-      composer.addPass(sceneRenderPass);
-      composer.addPass(unrealPass);
-      composer.addPass(toneMappingPass);
-      composer.addPass(vignettePass);
-    }
-
-    composer.render();
-  }, 1);
-
-  return (
-    <>
-      <ambientLight intensity={0.2} />
-      <directionalLight intensity={0.5} position={[20, 0, -10]} />
-      <PerspectiveCamera position-z={30} makeDefault />
-      <OrbitControls />
-
-      {/* A mesh with emission */}
-      <mesh position={[-14, 0, 0]}>
-        <dodecahedronGeometry />
-        <meshStandardMaterial
-          color="white"
-          emissive="white"
-          emissiveIntensity={1}
-        />
-      </mesh>
-
-      {/* A mesh with no emission */}
-      <mesh position={[+14, 0, 0]}>
-        <dodecahedronGeometry />
-        <meshStandardMaterial color="white" />
-      </mesh>
-
-      <Spaceship />
-    </>
-  );
 };
 
 export const App = () => (
@@ -92,6 +31,41 @@ export const App = () => (
     }}
   >
     <Skybox />
-    <Scene />
+
+    <ambientLight intensity={0.5} />
+    <directionalLight intensity={0.5} position={[20, 0, -10]} />
+    <PerspectiveCamera position-z={30} makeDefault />
+    <OrbitControls />
+
+    {/* A mesh with emission */}
+    <mesh position={[-14, 0, 0]}>
+      <dodecahedronGeometry />
+      <meshStandardMaterial
+        color="white"
+        emissive="white"
+        emissiveIntensity={1}
+      />
+    </mesh>
+
+    {/* A mesh with no emission */}
+    <mesh position={[+14, 0, 0]}>
+      <dodecahedronGeometry />
+      <meshStandardMaterial color="white" />
+    </mesh>
+
+    <Spaceship />
+
+    <Effects disableGamma encoding={LinearEncoding} type={HalfFloatType}>
+      <unrealBloomPass
+        args={[
+          new Vector2(window.innerWidth, window.innerHeight),
+          1.5,
+          0.0,
+          0.8,
+        ]}
+      />
+      <adaptiveToneMappingPass args={[true, 256]} />
+      <shaderPass args={[VignetteShader]} />
+    </Effects>
   </Canvas>
 );
